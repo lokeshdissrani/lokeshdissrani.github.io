17e0ad314da67bcd9d81ee2ab70aabe5b8e91f9c
lokeshdissrani <lokeshdissrani@gmail.com>
2020-08-19 12:14:59 +0530

    Updates

---
+++ precache-manifest.2005d0b7446bf2426d77ac5693e401cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b7864264bd66bf723fe931139a9bd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebd5b9b1dad6ae9b063c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb9ace65efbd45cda507",
+    "url": "/static/js/2.cee2fb5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cee2fb5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd5b9b1dad6ae9b063c",
+    "url": "/static/js/main.15c1b00d.chunk.js"
+  },
+  {
+    "revision": "5136b2ba23971aae115e",
+    "url": "/static/js/runtime-main.3cb32e42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
